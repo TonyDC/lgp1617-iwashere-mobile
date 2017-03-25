fccfd398444a93092be43e9930aa42af9381ac0b
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2017-03-25 23:31:48 +0000

    Initial setup

---
+++ app/src/main/java/com/teamc/mira/iwashere/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teamc.mira.iwashere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
