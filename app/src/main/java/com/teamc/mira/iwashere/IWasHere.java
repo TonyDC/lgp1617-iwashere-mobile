e5512c5cc4940bf3725bf3aa697a5f85bbfff2f4
Lukáš Konkoľ <luk.konkol@gmail.com>
2017-03-31 18:33:10 +0100

    Firebase setup, Google Sign in, layout setup

--- app/src/main/java/com/teamc/mira/iwashere/IWasHere.java
+++ app/src/main/java/com/teamc/mira/iwashere/IWasHere.java
@@ -1,27 +1,42 @@
 package com.teamc.mira.iwashere;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.teamc.mira.iwashere.presentation.login.LoginActivity;
 import com.teamc.mira.iwashere.presentation.main.MainActivity;
+import com.teamc.mira.iwashere.presentation.register.RegisterActivity;
 
 /*
  * This class is a Splash Screen
  */
-public class IWasHere extends AppCompatActivity {
+public class IWasHere extends AppCompatActivity implements View.OnClickListener {
 
     public final static String TAG = IWasHere.class.getCanonicalName();
-
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iwashere);
 
+        // Buttons
+        findViewById(R.id.main_sign_in_btn).setOnClickListener(this);
+        findViewById(R.id.main_sign_up_btn).setOnClickListener(this);
 
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(IWasHere.this, MainActivity.class));
+            finish();
+        }
+
+        /**
         Intent intent;
-
 
         if(getSharedPreferences("userInfo",MODE_PRIVATE).getString("token", null) != null){
             intent = new Intent(this, MainActivity.class);
@@ -29,7 +44,17 @@ public class IWasHere extends AppCompatActivity {
             intent = new Intent(this, LoginActivity.class);
         }
         startActivity(intent);
-        finish();
+        finish();**/
+    }
 
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.main_sign_in_btn) {
+            startActivity(new Intent(this, LoginActivity.class));
+        } else if (i == R.id.main_sign_up_btn) {
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
+        finish();
     }
 }
--- app/src/main/java/com/teamc/mira/iwashere/presentation/login/ResetPasswordActivity.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/login/ResetPasswordActivity.java
@@ -53,21 +53,19 @@ public class ResetPasswordActivity extends AppCompatActivity {
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
-                auth.sendPasswordResetEmail(email)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
-                                }
+                auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                        }
 
-                                progressBar.setVisibility(View.GONE);
-                            }
-                        });
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
             }
         });
     }
-
 }
