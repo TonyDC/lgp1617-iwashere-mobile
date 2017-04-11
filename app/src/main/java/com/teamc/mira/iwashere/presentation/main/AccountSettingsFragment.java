556faf300ae429eabe26bab4baa6a58cb36480bd
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2017-04-11 18:08:10 +0100

    Fragment switching completed

---
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/main/AccountSettingsFragment.java
@@ -0,0 +1,10 @@
+package com.teamc.mira.iwashere.presentation.main;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Duart on 11/04/2017.
+ */
+
+class AccountSettingsFragment extends Fragment {
+}
--- app/src/main/java/com/teamc/mira/iwashere/presentation/main/MainActivity.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/main/MainActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,15 +27,51 @@ import com.teamc.mira.iwashere.R;
 import com.teamc.mira.iwashere.presentation.auth.LoginActivity;
 import com.teamc.mira.iwashere.presentation.auth.SignupActivity;
 
-/**
- * Created by Duart on 27/03/2017.
- */
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment map = new MapFragment();
+        final Fragment feed = new FeedFragment();
+        final Fragment search = new SearchFragment();
+        final Fragment accountSettings = new AccountSettingsFragment();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                    findViewById(R.id.bottom_navigation);
+
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flContainer, map).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_map:
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                            fragmentTransaction.replace(R.id.flContainer, map).commit();
+                            return true;
+                        case R.id.action_feed:
+                            fragmentTransaction = fragmentManager.beginTransaction();
+                            fragmentTransaction.replace(R.id.flContainer, feed).commit();
+                            return true;
+                        case R.id.action_camera:
+
+                        case R.id.action_search:
+                            fragmentTransaction = fragmentManager.beginTransaction();
+//                            fragmentTransaction.replace(R.id.flContainer, )
+                        case R.id.action_account_settings:
+
+                    }
+                    return true;
+                }
+            });
     }
 }
