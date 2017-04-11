4e2d0e8ebb432951fea069b8485d38c375ccc286
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2017-04-11 17:11:24 +0100

    Navigation bar implemented

--- app/src/main/java/com/teamc/mira/iwashere/presentation/main/MainActivity.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.teamc.mira.iwashere.presentation.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,5 +26,10 @@ import com.teamc.mira.iwashere.presentation.auth.SignupActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+    }
 }
