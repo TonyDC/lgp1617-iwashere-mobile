fe9c6574e822cd9704ca467c94f6d5e92c530d89
jmepg <j.estrada.gouveia@gmail.com>
2017-04-12 23:46:01 +0100

    simple img slider implemented

--- app/src/main/java/com/teamc/mira/iwashere/presentation/poi/PoiDetailActivity.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/poi/PoiDetailActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.teamc.mira.iwashere.R;
 
 /**
@@ -11,9 +13,32 @@ import com.teamc.mira.iwashere.R;
  */
 
 public class PoiDetailActivity extends AppCompatActivity {
+
+    SliderLayout sliderShow;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poi_detail);
+        
+        sliderShow = (SliderLayout) findViewById(R.id.slider);
+
+
+        TextSliderView textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Game of Thrones")
+                .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        sliderShow.addSlider(textSliderView);
+
+
     }
+
+    @Override
+    protected void onStop() {
+        sliderShow.stopAutoCycle();
+        super.onStop();
+    }
+
 }
