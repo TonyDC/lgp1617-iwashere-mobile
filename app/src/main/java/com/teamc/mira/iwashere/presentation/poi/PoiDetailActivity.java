bb4557d37648076be6b3dccb21b2a3b07974e757
jmepg <j.estrada.gouveia@gmail.com>
2017-04-15 19:42:26 +0100

    added scrolling, gridview

--- app/src/main/java/com/teamc/mira/iwashere/presentation/poi/PoiDetailActivity.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/poi/PoiDetailActivity.java
@@ -1,11 +1,17 @@
 package com.teamc.mira.iwashere.presentation.poi;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
@@ -20,6 +26,9 @@ import java.util.function.ToLongBiFunction;
 public class PoiDetailActivity extends AppCompatActivity {
 
     SliderLayout sliderShow;
+    TextView textDescription;
+    ImageView textAddress;
+    GridView photoGallery;
 
 
     @Override
@@ -34,13 +43,30 @@ public class PoiDetailActivity extends AppCompatActivity {
 
         sliderShow = (SliderLayout) findViewById(R.id.slider);
 
-
         TextSliderView textSliderView = new TextSliderView(this);
         textSliderView
                 .description("Game of Thrones")
                 .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
         sliderShow.addSlider(textSliderView);
+
+        textDescription = (TextView) findViewById(R.id.description);
+        textDescription.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales suscipit venenatis. Sed ut rhoncus mi. Curabitur nec scelerisque ipsum. Fusce et diam eros. Pellentesque vel dolor ante. Suspendisse convallis diam nec eleifend tincidunt. Etiam vestibulum mi elit. Sed egestas tellus mattis, fermentum turpis eu, gravida neque. Sed at turpis ultricies, laoreet purus sed, sollicitudin urna. Donec diam ex, porta quis sollicitudin id, sollicitudin id urna. Nunc commodo rutrum odio sit amet viverra. Vestibulum blandit euismod efficitur. Nunc sit amet hendrerit enim. Pellentesque id diam lacus. Etiam vitae tellus sed turpis suscipit laoreet. Integer commodo in nulla nec vehicula.");
+
+        textAddress = (ImageView) findViewById(R.id.address);
+
+        textAddress.setBackgroundColor(Color.parseColor("#35A8DF"));
+
+        photoGallery = (GridView) findViewById(R.id.photogallery);
+        photoGallery.setAdapter(new ImageAdapter(this));
+
+        photoGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(PoiDetailActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
