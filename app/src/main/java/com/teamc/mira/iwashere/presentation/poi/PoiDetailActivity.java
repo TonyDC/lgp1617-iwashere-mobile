06c98bfc3e9f6e14d677bc43a5c51195967f5582
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2017-04-15 17:41:19 +0100

    Toolbar added

--- app/src/main/java/com/teamc/mira/iwashere/presentation/poi/PoiDetailActivity.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/poi/PoiDetailActivity.java
@@ -1,12 +1,17 @@
 package com.teamc.mira.iwashere.presentation.poi;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.teamc.mira.iwashere.R;
+
+import java.util.function.ToLongBiFunction;
 
 /**
  * Created by Duart on 12/04/2017.
@@ -21,7 +26,12 @@ public class PoiDetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poi_detail);
-        
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         sliderShow = (SliderLayout) findViewById(R.id.slider);
 
 
