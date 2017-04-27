3b6816288237a7967fff727f1c02af17de8106fb
jmepg <j.estrada.gouveia@gmail.com>
2017-04-27 16:44:26 +0100

    fix

--- app/src/main/java/com/teamc/mira/iwashere/presentation/camera/CameraInit.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/camera/CameraInit.java
@@ -3,28 +3,49 @@ package com.teamc.mira.iwashere.presentation.camera;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.teamc.mira.iwashere.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class CameraInit extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
+    private Button photoButton;
+    private Uri imageToUploadUri;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera);
-        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
-        Button photoButton = (Button) this.findViewById(R.id.button1);
+        imageView = (ImageView) this.findViewById(R.id.picturedisplay);
+        photoButton = (Button) this.findViewById(R.id.newPhoto);
+
         photoButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                File f = new File(Environment.getExternalStorageDirectory(), "POST_IMAGE.jpg");
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                imageToUploadUri = Uri.fromFile(f);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
         });
@@ -32,8 +53,47 @@ public class CameraInit extends Activity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // Bitmap photo = (Bitmap) data.getExtras().get("data");
+            //imageView.setImageBitmap(photo);
+            Bitmap photo = getBitmap(imageToUploadUri);
             imageView.setImageBitmap(photo);
+
         }
+    }
+
+    private Bitmap getBitmap(Uri uri) {
+        InputStream inputStream = null;
+        try {
+            inputStream = getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Rect rect = new Rect(0, 0, 0, 0);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        options.inSampleSize = getInSampleSize(options, 128, 128);
+
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream, rect, options);
+        return bitmap;
+    }
+
+    private int getInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int width = options.outWidth;
+        int height = options.outHeight;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            int halfHeight = height / 2;
+            int halfWidth = width / 2;
+
+            while ((halfHeight / inSampleSize) > reqHeight &&
+                    (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
