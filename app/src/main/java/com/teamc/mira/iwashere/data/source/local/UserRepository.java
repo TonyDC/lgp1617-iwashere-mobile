77ee8c400ccb3ca1b74e2c6bc183e70c505f685e
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2017-05-03 19:51:30 +0100

    Fixed fetch of Token

--- app/src/main/java/com/teamc/mira/iwashere/data/source/local/UserRepository.java
+++ app/src/main/java/com/teamc/mira/iwashere/data/source/local/UserRepository.java
@@ -1,9 +1,17 @@
 package com.teamc.mira.iwashere.data.source.local;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Duart on 03/05/2017.
@@ -26,7 +34,21 @@ public class UserRepository implements com.teamc.mira.iwashere.domain.repository
 
     @Override
     public String getToken() {
-        return FirebaseInstanceId.getInstance().getToken();
+        final StringBuilder token = new StringBuilder() ;
+        final CountDownLatch countDownLatch = new CountDownLatch(1) ;
+        FirebaseAuth.getInstance().getCurrentUser().getToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+            @Override
+            public void onComplete(@NonNull Task<GetTokenResult> task) {
+                token.append(task.getResult().getToken());
+                countDownLatch.countDown();
+            }
+        });
+        try {
+            countDownLatch.await(5, TimeUnit.SECONDS);
+            return token.toString() ;
+        } catch (InterruptedException ie) {
+            return null;
+        }
     }
 
     @Override
