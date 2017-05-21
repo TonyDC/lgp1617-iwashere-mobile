5df6b12f2516be095df1782f76c6d89a9f952100
Duarte Manuel Ribeiro Pinto <duartemr.pinto@gmail.com>
2017-05-21 12:42:31 +0100

    Fixed bug where context was null

--- app/src/main/java/com/teamc/mira/iwashere/presentation/main/MapFragment.java
+++ app/src/main/java/com/teamc/mira/iwashere/presentation/main/MapFragment.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -23,7 +25,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     protected MapView mMapView;
     protected GoogleMap mGoogleMap;
-    protected Context mContext = getContext();
+    protected Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+    }
 
     protected void checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
